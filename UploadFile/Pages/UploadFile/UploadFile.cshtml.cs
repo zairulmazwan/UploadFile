d8b20b4422c33b701fee389d3986d8237bb0dda8
zm1142 <zm1142@STA-DK-400392>
2020-10-27 15:16:12 +0000

    Upload File

--- UploadFile/Pages/UploadFile/UploadFile.cshtml.cs
+++ UploadFile/Pages/UploadFile/UploadFile.cshtml.cs
@@ -5,33 +5,43 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
 namespace UploadFile.Pages.UploadFile
 {
     public class UploadFileModel : PageModel
     {
 
-        private readonly IWebHostEnvironment _env;
+        private readonly IHostingEnvironment _webHostEnvironment;
 
-        public UploadFileModel (IWebHostEnvironment env)
+        public UploadFileModel (IHostingEnvironment env)
         {
-            _env = env;
+            _webHostEnvironment = env;
         }
 
 
 
         [BindProperty]
-        public IFormFile FileName { get; set; }
+        public IFormFile UserFile { get; set; }
 
         
         public void OnGet()
         {
+            Console.WriteLine("The page of upload is loaded");
         }
 
-        public void OnPost()
+        public async Task<IActionResult> OnPostAsync()
         {
-            var FileToUpload = PathString.Combine(_env.WebRootPath, )
+            var FileToUpload = Path.Combine(_webHostEnvironment.WebRootPath, "images", UserFile.FileName);
+            Console.WriteLine("File Name : "+FileToUpload);
+
+            using (var FStream = new FileStream (FileToUpload, FileMode.Create))
+            {
+                await UserFile.CopyToAsync(FStream);
+            }
+
+            return Page();
         }
        
     }
