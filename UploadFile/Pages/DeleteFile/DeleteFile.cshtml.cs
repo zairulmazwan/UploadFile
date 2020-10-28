ed5515b2576d84c525a0995bb3189eb5a90e8908
informata <informata@informata.es>
2020-10-28 22:15:42 +0100

    File explorer and delete

--- UploadFile/Pages/DeleteFile/DeleteFile.cshtml.cs
+++ UploadFile/Pages/DeleteFile/DeleteFile.cshtml.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
 namespace UploadFile.Pages.DeleteFile
 {
@@ -13,12 +14,36 @@ namespace UploadFile.Pages.DeleteFile
     {
         [BindProperty]
         public int NoFiles { get; set; }
-        public void OnGet()
-        {
 
-            string path = "/images/";
-            NoFiles = Directory.GetFiles(path, "*", SearchOption.AllDirectories).Length;
-            
+        [BindProperty]
+        public List<string> FilesList { get; set; }
+
+        private readonly IHostingEnvironment _webHostEnvironment;
+
+        public DeleteFileModel(IHostingEnvironment env)
+		{
+            _webHostEnvironment = env;
+        }
+
+
+        public void OnGet(string fileToDelete = "")
+        {
+            string imageFolder = "images";
+            string path = Path.Combine(_webHostEnvironment.WebRootPath, imageFolder);
+
+            // Delete the file on the parameter
+            if(fileToDelete != "")
+                System.IO.File.Delete(Path.Combine(path, fileToDelete));
+
+            // List
+            var PathsList = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
+            NoFiles = PathsList.Length;
+
+            FilesList = new List<string>();
+            foreach(var FilePath in PathsList)
+			{
+                FilesList.Add(Path.GetFileName(FilePath));
+			}
         }
     }
 }
